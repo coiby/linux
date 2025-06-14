bb1404787edd9618bede15b24bb7d7eef2ae1049
Coiby Xu <coxu@redhat.com>
2025-06-14 09:50:43 +0800

    of, crash: pass dm-crypt keys to kdump kernel
    1st kernel will add device tree property dmcryptkeys as similar to
    elfcorehdr to pass the memory address of the stored info of dm-crypt
    keys to kdump kernel.

--- drivers/of/kexec.c
+++ drivers/of/kexec.c
@@ -423,12 +423,28 @@ void *of_kexec_alloc_and_setup_fdt(const struct kimage *image,
 		if (ret)
 			goto out;
 
+		ret = fdt_appendprop_addrrange(fdt, 0, chosen_node,
+				"linux,dmcryptkeys", image->dm_crypt_keys_addr,
+				image->dm_crypt_keys_sz);
+
+		if (ret)
+			goto out;
+
 		/*
 		 * Avoid elfcorehdr from being stomped on in kdump kernel by
 		 * setting up memory reserve map.
 		 */
 		ret = fdt_add_mem_rsv(fdt, image->elf_load_addr,
 				      image->elf_headers_sz);
+		if (ret)
+			goto out;
+
+		/*
+		 * Avoid dmcryptkeys from being stomped on in kdump kernel by
+		 * setting up memory reserve map.
+		 */
+		ret = fdt_add_mem_rsv(fdt, image->dm_crypt_keys_addr,
+				      image->dm_crypt_keys_sz);
 		if (ret)
 			goto out;
 
